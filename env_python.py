2f6c216a8d81d2eded7766201f091288dd45d313
monu lakshkar <monu@k2io.com>
2022-05-13 16:36:46 +0530

    Initial commit

---
+++ env_python.py
@@ -0,0 +1,19 @@
+import ssh
+
+# configuration
+INSTANA_CONTAINER_NAME = "instana/agent"
+INSTANA_CMD = 'docker run --detach --name instana-agent --volume /var/run/docker.sock:/var/run/docker.sock --volume /dev:/dev --volume /sys:/sys --volume /var/log:/var/log --privileged --net=host --pid=host --ipc=host --env="INSTANA_AGENT_KEY=i6anpVVIQPyPvPqdcKxM8w" --env="INSTANA_AGENT_ENDPOINT=saas-us-west-2.instana.io" --env="INSTANA_AGENT_ENDPOINT_PORT=443" instana/agent'
+K2_INSTALL_CMD = "bash /root/installer/k2install/k2install.sh -i micro-agent"
+APP_INSTALL_WITH_CMD = "docker run -v /opt/k2root:/opt/k2root:z -e K2_OPTS='-m k2_python_agent' -e PYTHONPATH=/opt/k2root/k2root/collectors/k2-python-agent/lib/site-packages -e K2_GROUP_NAME=IAST -itd -p 8000:8000 --name syscalls_python k2cyber/test_application:syscalls_python"
+APP_INSTALL_WITHOUT_CMD = "docker run -itd -p 8000:8000 --name syscalls_python k2cyber/test_application:syscalls_python"
+APP_CONTAINER_NAME = "syscalls_python"
+WITH_YANDEX_NAME = "yandex-python-with"
+WITHOUT_YANDEX_NAME = "yandex-python-without"
+YANDEX_WITH_DIR = "/root/longevity/python/with"
+YANDEX_WITHOUT_DIR = "/root/longevity/python/without"
+WITH_MACHINE = ssh.User("192.168.5.138","root","k2cyber")
+WITHOUT_MACHINE = ssh.User("192.168.5.136","root","k2cyber")
+LOAD_MACHINE = ssh.User("192.168.5.62","root","k2cyber")
+FIRST_CURL = 'curl "http://localhost:8000/file/file_view_read"'
+APP_DETECT_TXT = "Starting development server"
+APP_PORT = "8000"
---
+++ start_longevity.py
@@ -0,0 +1,298 @@
+import datetime
+import getopt
+import os
+import re
+import sys
+import threading
+import time
+import ssh
+
+def detectContainer(user: ssh.User, containerName):
+    endTime = datetime.datetime.now() + datetime.timedelta(minutes=15)
+    count = 1
+    id = ""
+    while True:
+        if datetime.datetime.now() >= endTime:
+            break
+        print(f"Detecting running {containerName} container at {user.ip}... ",count)
+        cmd = "docker ps -a | grep '%s' | awk '{print $1}'" % (containerName)
+        id=ssh.doSSH(user,cmd)
+        id=id.replace("\n","")
+        if id!="" and id!=None:
+            print(f"{containerName} started successfully.")
+            return True
+        count+=1
+    print("No running container found.")
+    return False
+
+def detectContainerFromLogs(user: ssh.User, id, detect_txt):
+    endTime = datetime.datetime.now() + datetime.timedelta(minutes=15)
+    count = 1
+    while True:
+        if datetime.datetime.now() >= endTime:
+            break
+        print(f"Detecting running {id} container at {user.ip}... ",count)
+        cmd = "docker logs '%s'" % (id)
+        out=ssh.doSSH(user,cmd)
+        out=out.replace("\n","")
+        if detect_txt in out:
+            print(f"{id} started successfully.")
+            return True
+        count+=1
+    print("No running container found.")
+    return False
+
+def getAndRemoveContainer(user: ssh.User,containerName,remove=True):
+    print(f"Detecting running {containerName} container at {user.ip}")
+    cmd = "docker ps -a | grep '%s' | awk '{print $1}'" % (containerName)
+    # print(cmd)
+    id=""
+    id=ssh.doSSH(user,cmd)
+    id=id.replace("\n","")
+    out=re.findall("[^\s]+", id)
+    if len(out) > 1:
+        return True, None
+
+    if id!="" and id!=None:
+        print("Detected:: ",id)
+        if remove:
+            cmd = f"docker rm -f {id}"
+            out=ssh.doSSH(user,cmd)
+            print(f"Container {containerName} stopped.", out)
+            return True, id
+        else:
+            return False, id
+    else:
+        print(f"No running container found with {containerName}.")
+        return True, None
+
+def attachApplication(user: ssh.User,id):
+    print(f"Started application attachment...")
+    cmd = f"docker exec {id} bash -c 'bash /opt/k2root/k2root/collectors/k2-php-agent/k2_php_agent_install_script.sh --allow-server-restart=TRUE'"
+    # print(cmd)
+    out = ssh.doSSH(user,cmd)
+    out=out.replace("\n","")
+    if "PHP collector for apache server activated" in out:
+        print(f"PHP collector for apache server activated.")
+        return True
+    else:
+        return False
+
+def startRSSMonitor(user: ssh.User,id):
+    print(f"Starting RSS monitoring in {id} container at {user.ip}...")
+    cmd = f"docker exec -d {id} bash -c 'cd rss; nohup bash rss.sh &> output & sleep 1'"
+    # print(cmd)
+    ssh.doSSH(user,cmd)
+    print("RSS monitoring Started!")
+    makeRequest(user)
+
+def makeRequest(user: ssh.User):
+    out=ssh.doSSH(user,env.FIRST_CURL)
+    print("First request fired!")
+
+def startWith(withMachine):
+    print(f"\n## WITH AGENT")
+    if getAndRemoveContainer(withMachine,env.INSTANA_CONTAINER_NAME):
+        print(f"Launching instana agent on {withMachine.ip}")
+        ssh.doSSH(withMachine,env.INSTANA_CMD)
+        if detectContainer(withMachine,env.INSTANA_CONTAINER_NAME):
+            print(f"Launching k2 agent on {withMachine.ip}")
+            ssh.doSSH(withMachine,env.K2_INSTALL_CMD)
+            print(f"K2 agent started.\nStarting application with k2 agent on {withMachine.ip}")
+            appStatus, appContainerId = getAndRemoveContainer(withMachine,env.APP_CONTAINER_NAME)
+            if appStatus:
+                id=ssh.doSSH(withMachine,env.APP_INSTALL_WITH_CMD)
+                id=id.replace("\n","")
+                out=re.findall("[^\s]+", id)
+                # print(out)
+                if len(out) == 1 and id!="" and id!=None:
+                    print("Detected:: ",id)
+                    if detectContainerFromLogs(withMachine,id,env.APP_DETECT_TXT):
+                        if lang == "php":
+                            if attachApplication(withMachine,id):
+                                startRSSMonitor(withMachine,id)
+                        elif lang == "node" or lang == "python":
+                            makeRequest(withMachine)
+
+def startWithout(withoutMachine):
+    print(f"\n## WITHOUT AGENT")
+    if getAndRemoveContainer(withoutMachine,env.INSTANA_CONTAINER_NAME):
+        print(f"Launching instana agent on {withoutMachine.ip}")
+        ssh.doSSH(withoutMachine,env.INSTANA_CMD)
+        if detectContainer(withoutMachine,env.INSTANA_CONTAINER_NAME):
+            print(f"Starting application without k2 agent on {withoutMachine.ip}")
+            appStatus, appContainerId = getAndRemoveContainer(withoutMachine,env.APP_CONTAINER_NAME)
+            if appStatus:
+                id=ssh.doSSH(withoutMachine,env.APP_INSTALL_WITHOUT_CMD)
+                id=id.replace("\n","")
+                out=re.findall("[^\s]+", id)
+                if len(out) == 1 and id!="" and id!=None:
+                    print("Detected:: ",id)
+                    if detectContainerFromLogs(withoutMachine,id,env.APP_DETECT_TXT):
+                        if lang == "php":
+                            startRSSMonitor(withoutMachine,id)
+                        elif lang == "node" or lang == "python":
+                            makeRequest(withoutMachine)
+
+def startRequestFiring(machine,containerName,dir):
+    if getAndRemoveContainer(machine,containerName):
+        print(f"Starting {containerName} container at {machine.ip}...")
+        cmd = f"cd {dir}; docker run -itd -v $(pwd):/var/loadtest -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent --net host --name {containerName} direvius/yandex-tank"
+        # print(cmd)
+        ssh.doSSH(machine,cmd)
+        print("Started!")
+
+def updateLoadFile(user: ssh.User, dir, fileName, tmpName):
+    out = ssh.doSCP(env.LOAD_MACHINE,tmpName,f"{dir}/load.yaml",False)
+    # print(out)
+
+    if os.path.exists(fileName):
+        os.remove(fileName)
+    with open(tmpName, 'r+') as fp:
+        f = open(fileName, "w")
+        for c, line in enumerate(fp):
+            if "address:" in line and f"{user.ip}:{env.APP_PORT}" not in line:
+                # print("%s--->Update"%c)
+                line = "  address: %s:%s # [Target's address]:[target's port]\n"%(user.ip,env.APP_PORT)
+            f.write(line)
+        
+        if os.path.exists(tmpName):
+            os.remove(tmpName) 
+        f.close()
+    fp.close()
+    
+    out2 = ssh.doSCP(env.LOAD_MACHINE,fileName,f"{dir}/load.yaml")
+    # print(out2)
+    if os.path.exists(fileName):
+        os.remove(fileName)
+
+def cleanMachine(user: ssh.User):
+    cmd = f"docker rm -f $(docker ps -aq) && rm -rf /opt/k2root"
+    doClean(user,cmd)
+
+def doClean(user: ssh.User, cmd):
+    print(f"Cleaning up {user.ip}...")
+    # print(cmd)
+    ssh.doSSH(user,cmd)
+    print(f"{user.ip} is Cleaned!")
+
+def doCleanUp():
+    # clean apps and k2
+    cleanMachine(env.WITH_MACHINE)
+    cleanMachine(env.WITHOUT_MACHINE)
+    # clean yandex
+    getAndRemoveContainer(env.LOAD_MACHINE, env.WITH_YANDEX_NAME)
+    getAndRemoveContainer(env.LOAD_MACHINE, env.WITHOUT_YANDEX_NAME)
+    # clean yandex logs
+    doClean(env.WITH_MACHINE,f"rm -rf {env.YANDEX_WITH_DIR}/logs/*")
+    doClean(env.WITHOUT_MACHINE,f"rm -rf {env.YANDEX_WITHOUT_DIR}/logs/*")
+
+
+def pickEnv():
+    global lang
+    clean = False
+    lang = None
+    argv = sys.argv[1:]
+
+    try:
+        opts, args = getopt.getopt(argv, "l:c:h",["language=","clean=","help="])
+    except:
+        showHelp()
+        return
+
+    for opt, arg in opts:
+        if opt in ['-l','--language']:
+            lang = arg
+        if opt in ['-c','--clean']:
+            lang = arg
+            clean = True
+
+    global env
+    match lang:
+        case "node":
+            import env_node as env
+        case "python":
+            import env_python as env
+        case "php":
+            import env_php as env
+        case default:
+            showHelp()
+            return
+            
+    print("\n{:<30} {:<40}".format("Lanaguage collector :: ",lang))
+    print("{:<30} {:<40}".format("Machine with K2 :: ",env.WITH_MACHINE.ip))
+    print("{:<30} {:<40}".format("Machine without K2 :: ",env.WITHOUT_MACHINE.ip))
+    print("{:<30} {:<40}".format("K2 agent install command:: ",env.K2_INSTALL_CMD))
+    print("{:<30} {:<40}".format("App install command (with K2):: ",env.APP_INSTALL_WITH_CMD))
+    print("{:<30} {:<40}".format("App install command:: ",env.APP_INSTALL_WITHOUT_CMD))
+    answer = input("Continue (y/yes or n/no)?\t\t")
+    if answer in ['y', 'yes']:
+        if clean:
+            doCleanUp()
+        else:
+            startUp()
+    else:
+        print("Update these values in respective env file!")
+
+#here is the animation
+def animate():
+    global done 
+    done = False
+    while not done:
+        sys.stdout.write('\rloading | ')
+        time.sleep(0.07)
+        sys.stdout.write('\rloading / ')
+        time.sleep(0.08)
+        sys.stdout.write('\rloading - ')
+        time.sleep(0.07)
+        sys.stdout.write('\rloading \\ ')
+        time.sleep(0.07)
+
+def showHelp():
+    print(f"Usage: python3 {os.path.basename(__file__)} <OPTIONS>")
+    print("OPTIONS:")
+    print("{:<30} {:<40}".format("-l/--language <argument>","Start longevity for specified language"))
+    print("{:<30} {:<40}".format("","Valid argument with -l are:"))
+    print("{:<30} {:<40}".format("","node - to start longevity for Node agent"))
+    print("{:<30} {:<40}".format("","python - to start longevity for Python agent"))
+    print("{:<30} {:<40}".format("","php - to start longevity for PHP agent"))
+    print(f"\nNOTE: keep the K2 agent installer in '/root/installer/'")
+
+def startUp():
+    print(f"*** Make sure VPN is connected ***")
+    threading.Thread(target=animate).start()
+    threadWith = threading.Thread(target=startWith,args=(env.WITH_MACHINE,))
+    threadWithout = threading.Thread(target=startWithout,args=(env.WITHOUT_MACHINE,))
+
+    threadWith.start()
+    threadWithout.start()
+
+    threadWith.join()
+    threadWithout.join()
+    print("Longevity application setup complete!!")
+
+    print("Updating load.yaml file...")
+    threadFileWith = threading.Thread(target=updateLoadFile,args=(env.WITH_MACHINE,env.YANDEX_WITH_DIR,"load_with.yaml","temp_with.yaml"))
+    threadFileWithout = threading.Thread(target=updateLoadFile,args=(env.WITHOUT_MACHINE,env.YANDEX_WITHOUT_DIR,"load_without.yaml","temp_without.yaml"))
+
+    threadFileWith.start()
+    threadFileWithout.start()
+
+    threadFileWith.join()
+    threadFileWithout.join()
+
+    print("Starting request firing...")
+    threadFiringWith = threading.Thread(target=startRequestFiring,args=(env.LOAD_MACHINE,env.WITH_YANDEX_NAME,env.YANDEX_WITH_DIR))
+    threadFiringWithout = threading.Thread(target=startRequestFiring,args=(env.LOAD_MACHINE,env.WITHOUT_YANDEX_NAME,env.YANDEX_WITHOUT_DIR))
+
+    threadFiringWith.start()
+    threadFiringWithout.start()
+
+    threadFiringWith.join()
+    threadFiringWithout.join()
+    global done 
+    done = True
+    print("Longevity started successfully!!")
+
+if __name__ == "__main__":
+    pickEnv()
